85de68f48bde1c1b6a81027f6125cfb3b990dd10
John_Duffy <JohnDuffy271@gmail.com>
2026-07-11 12:52:15 +0100

    Fix mobile phone menu issues

--- generatepress-child/functions.php
+++ generatepress-child/functions.php
@@ -98,13 +98,13 @@ function mbc_front_page_assets() {
             'mbc-split-layout',
             get_stylesheet_directory_uri() . '/assets/split-layout.css',
             array( 'mbc-shared' ),
-            '1.0.5'
+            '1.0.6'
         );
         wp_enqueue_script(
             'mbc-split-layout',
             get_stylesheet_directory_uri() . '/assets/split-layout.js',
             array( 'mbc-shared' ),
-            '1.0.5',
+            '1.0.6',
             true
         );
     }
